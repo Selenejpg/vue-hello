45dc1377875af3efe871c0ac4351bf3958b5872b
noemipintaldi <noemipintaldi@gmail.com>
2022-03-30 16:50:00 +0200

    bonus completato

--- myscript.js
+++ myscript.js
@@ -2,5 +2,8 @@ var app = new Vue({
         el: "#root",
         data: {
             message: "Hello World!",
+            doggo: {
+                img: 'this-is-fine-question-hound.jpeg'
+            }
         }    
 });
